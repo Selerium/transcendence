4624d1d3efe09a2a72a455abc3f8499cbaa66142
juhaamid <juhaamid@student.42abudhabi.ae>
2024-11-04 15:29:23 +0400

    trial

--- django/django_script.sh
+++ django/django_script.sh
@@ -4,3 +4,15 @@ django-admin startproject backend .
 
 python manage.py startapp hello
 
+python manage.py startapp newyears
+
+cd /usr/src/app/hello 
+
+mkdir templates
+mkdir templates/hello
+
+cd ..
+cd /usr/src/app/newyears
+mkdir templates
+mkdir templates/newyears
+touch index.html
