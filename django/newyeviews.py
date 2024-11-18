f56f392e9360f435bfd5a32e68d5462619c4f471
jens8n <jensen.bucoy@gmail.com>
2024-11-18 09:29:56 +0400

    django set up WIP

--- django/newyeviews.py
+++
@@ -1,9 +0,0 @@
-import datetime
-from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-	now = datetime.datetime.now()
-	return render(request, "newyears/index.html",{
-		"newyears": now.month = 1 and now.day = 1
-	})
