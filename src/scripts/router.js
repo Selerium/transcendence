e7fc6282b4f8cd88f8493aba77f61b473b80cb72
John Adithya <jadithya@42student.abudhabi.ae>
2025-01-13 13:08:57 +0400

    url changes accordingly

--- src/scripts/router.js
+++ src/scripts/router.js
@@ -7,33 +7,37 @@ routes = {
   "/profile": "/profile.html",
 };
 
-window.addEventListener('click', (e) => {
-  e.preventDefault();
-  if (e.target.tagName != "A")
-    return ;
-
-  changeRoute(e.target.getAttribute('href'));
-});
-
 // to write the 404 condition here
-async function changeRoute(path) {
-  const main = document.getElementById('main');
-  const html = await fetch(routes[path])
-  .then((response) => response.text());
+async function changeRoute() {
+  const path = window.location.pathname;
 
-  main.style.transform = 'translateX(25%)';
+  const main = document.getElementById("main");
+  const html = await fetch(routes[path]).then((response) => response.text());
+
+  main.style.transform = "translateX(25%)";
   main.style.opacity = 0;
-  main.style.filter = 'blur(16px)';
-  
+  main.style.filter = "blur(16px)";
+
   setTimeout(() => {
     main.innerHTML = html;
-    main.style.transform = 'translateX(0%)';
+    main.style.transform = "translateX(0%)";
     main.style.opacity = 1;
-    main.style.filter = '';
+    main.style.filter = "";
   }, 200);
 }
+
+window.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.tagName != "A") return;
+
+  window.history.pushState({}, "", e.target.getAttribute("href"));
+  changeRoute(e.target.getAttribute("href"));
+});
 
 function checkLogin() {
   // if authenticated, go to "/dashboard"
   // else, go to "/login"
 }
+
+window.onpopstate = changeRoute();
+changeRoute();
