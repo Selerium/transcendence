ed2ad587353bb75907c8158d7de25152537b563a
John Adithya <jadithya@42student.abudhabi.ae>
2025-01-21 17:34:56 +0400

    finishing beepboop root api

--- django/app/api/views.py
+++ django/app/api/views.py
@@ -1,27 +1,32 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 from .serializers import UserSerializer
-from django.http import Http404
 
 from .models import User
 
-@api_view(['GET', 'POST'])
+ERROR400 = Response(data={'success': False, 'message': 'Invalid fields'}, status=status.HTTP_400_BAD_REQUEST)
+ERROR404 = Response(data={'success': False, 'message': 'Not Found'}, status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
 def users(request, id=None):
-    if (request.method == 'GET' and id == None):
-        users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    if (request.method == 'GET'):
+        # GET all users' info
+        if (id is None):
+            users = User.objects.filter(deleted=False)
+            serializer = UserSerializer(users, many=True)
+            return Response(data={'success': True, 'data': serializer.data}, status=status.HTTP_200_OK)
+        # GET one user's info
+        else:
+            user = get_object_or_404(User, id=id, deleted=False)
+            serializer = UserSerializer(user)
+            return Response(data={'success': True, 'data': serializer.data}, status=status.HTTP_200_OK)
 
-    elif (request.method == 'GET'):
-        try:
-            serializer = UserSerializer(User.objects.get(id=id))
-        except:
-            return Response(data={'success': False, 'message': 'Not Found'}, status=status.HTTP_404_NOT_FOUND)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    elif (request.method == 'POST' and id == None):
+    # POST and create a new user
+    elif (request.method == 'POST' and id is None):
         data = request.data
         newUser = User(
             username=data.get('username'),
@@ -31,15 +36,29 @@ def users(request, id=None):
         try:
             newUser.full_clean()
             newUser.save()
-            return Response(data={'success': True, 'message': 'Succesfully added user'}, status=status.HTTP_200_OK)
+            serializer = UserSerializer(newUser)
+            return Response(data={'success': True, 'data': serializer.data}, status=status.HTTP_200_OK)
         except:
-            return Response(data={'success': False, 'message': 'Invalid fields'}, status=status.HTTP_400_BAD_REQUEST)
+            return ERROR400
 
+    # PUT an update for one user
     elif (request.method == 'PUT' and not id == None):
-        try:
-            serializer = UserSerializer(User.objects.get(id=id), data=request.data)
-        except:
-            return Response(data={'success': False, 'message': 'Invalid fields'}, status=status.HTTP_400_BAD_REQUEST)
+        user = get_object_or_404(User, id=id, deleted=False)
+        serializer = UserSerializer(user, data=request.data)
         if (serializer.is_valid()):
             serializer.save();
-            return Response(data={'success': True, 'message': 'Updated user'}, status=status.HTTP_200_OK)
+            return Response(data={'success': True, 'data': serializer.data}, status=status.HTTP_200_OK)
+        return ERROR400
+
+    # DELETE a specific user
+    elif (request.method == 'DELETE' and not id == None):
+        user = get_object_or_404(User, id=id)
+        user.deleted = True
+        user.username = '--redacted--'
+        user.profile_pic = ''
+        user.status = 0
+        user.save()
+        serializer = UserSerializer(user)
+        return Response(data={'success': True, 'data': serializer.data}, status=status.HTTP_200_OK)
+
+    return ERROR404
