3f285d55eb6d2844620555d85eb557b281430f01
John Adithya <jadithya@42student.abudhabi.ae>
2025-02-13 18:56:55 +0400

    cleaned up unnecessaries

--- run-postgres.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker rm database
-docker image rm trans_database
-docker build -t trans_database postgres
-docker run -d -p 5432:5432 --name database -v "$PWD"/postgres/data:/var/lib/postgresql/data --env-file postgres/.env --network trans_network trans_database
